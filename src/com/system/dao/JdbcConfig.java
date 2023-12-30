f978d0584c53a130d386e5056c111909020cad12
shasha333333 <63197886+shasha333333@users.noreply.github.com>
2023-12-30 20:35:14 +0800

    Add files via upload

---
+++ src/com/system/dao/JdbcConfig.java
@@ -0,0 +1,8 @@
+package com.system.dao;
+
+public interface JdbcConfig {
+    String DRIVER = "com.mysql.cj.jdbc.Driver";
+    String URL = "jdbc:mysql://localhost:3306/studentsystemdao";
+    String USERNAME = "root";
+    String PASSWORD = "admin";
+}
---
+++ src/com/system/entity/User.java
@@ -0,0 +1,23 @@
+package com.system.entity;
+
+public class User {
+    private String username;    //ÓÃ»§Ãû
+    private String password;    //ÃÜÂë
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
